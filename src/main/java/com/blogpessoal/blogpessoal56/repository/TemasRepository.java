ebf5479e3d675d72fe6d29c96ca13afe01f1f824
Gloower <gloowerzin@gmail.com>
2022-08-30 16:08:59 -0300

    Teste

--- src/main/java/com/blogpessoal/blogpessoal56/repository/TemasRepository.java
+++ src/main/java/com/blogpessoal/blogpessoal56/repository/TemasRepository.java
@@ -1,4 +1,4 @@
-package com.blogpessoal.blogpessoal56.blog.repository;
+package com.blogpessoal.blogpessoal56.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
--- src/main/java/com/blogpessoal/blogpessoal56/repository/UsuarioRepository.java
+++ src/main/java/com/blogpessoal/blogpessoal56/repository/UsuarioRepository.java
@@ -1,4 +1,4 @@
-package com.blogpessoal.blogpessoal56.blog.repository;
+package com.blogpessoal.blogpessoal56.repository;
 
 import com.blogpessoal.blogpessoal56.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
